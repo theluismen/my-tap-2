f9debc49231e659132431c5322cc462eff7ab8aa
Luis Manuel Ocampo <luismanuel.ocampo@estudiants.urv.cat>
2024-10-18 12:28:37 +0200

    added comment to Ex1

--- src/exercise1/Main.java
+++ src/exercise1/Main.java
@@ -18,6 +18,7 @@ public class Main {
         return filt;
     }
 
+    // Exercise 1: Implement filter function in Java.
     public static void main(String[] args) {
         int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
         Predicate<Integer> odd;
