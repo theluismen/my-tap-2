1d1f0e145cadfd9e0139fcbf7c079e763369ba6b
Luis Manuel Ocampo <luismanuel.ocampo@estudiants.urv.cat>
2024-10-18 12:17:35 +0200

    feat: Ejercicio 1 Hecho

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # TAP 2
 
 Este repositorio contiene mis implementaciones a los ejercicios propuestos en la TAP 2.
+
+## Ejercicios Hechos
+- [x] Ejercicio 1
+- [ ] Ejercicio 2
+- [ ] Ejercicio 3
+- [ ] Ejercicio 4
+- [ ] Ejercicio 5
+- [ ] Ejercicio 6
+- [ ] Ejercicio 7
+
---
+++ src/exercise1/Main.java
@@ -0,0 +1,45 @@
+package exercise1;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
+public class Main {
+
+    public static <T> List<T> filter ( List<T> list, Predicate<T> pred ) {
+        List<T> filt = new ArrayList<>();
+
+        for ( T elem: list ) {
+            if ( pred.test(elem) ) {
+                filt.add(elem);
+            }
+        }
+
+        return filt;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        Predicate<Integer> odd;
+        List<Integer> numsList = new ArrayList<>();
+        List<Integer> odds;
+
+        System.out.println("-- Exercise 1 --");
+        /* Añadir numeros del VECTOR a una List */
+        System.out.print("Nums: ");
+        for ( Integer i: nums ) {
+            System.out.print( i + " " );
+            numsList.add( i );
+        }
+        System.out.println();
+
+        /* Definir el Predicate y filtrar */
+        odd = x -> ( x%2 ) != 0;
+        odds = filter( numsList, odd );
+
+        System.out.print("Odds: ");
+        for ( Integer i: odds ) {
+            System.out.print( i + " " );
+        }
+    }
+}
